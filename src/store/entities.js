00b3fc397b281ce09a37e41d76ff288584fe1ff6
István Ábrahám <istvan.abraham@vicampo.de>
2024-08-23 00:11:58 +0200

    Logic

---
+++ src/store/entities.js
@@ -0,0 +1,50 @@
+import { addEntity, loadEntities, removeEntity, updateEntity } from '@/util/Handler'
+import { create } from 'zustand'
+
+const useEntityStore = create((set, get) => ({
+  entities: {},
+  getEnabled: () => {
+    const { entities } = get()
+    return Object.values(entities).filter(({ enabled }) => enabled)
+  },
+  fetch: async () => {
+    const entities = await loadEntities()
+
+    set({ entities })
+  },
+  add: async (name) => {
+    const newEntity = { name, enabled: true }
+    const { entities } = get()
+
+    try {
+      const id = await addEntity(newEntity)
+      set({ entities: { ...entities, [id]: newEntity } })
+    } catch (e) {
+      console.error(e)
+    }
+  },
+  remove: async (id) => {
+    const { entities } = get()
+
+    try {
+      await removeEntity(id)
+      const { [id]: _, ...updated } = entities
+      set({ entities: updated })
+    } catch (e) {
+      console.error(e)
+    }
+  },
+  update: async (id, enabled) => {
+    const { entities } = get()
+
+    const updated = { ...entities[id], enabled }
+    try {
+      await updateEntity(id, updated)
+      set({ entities: { ...entities, [id]: updated } })
+    } catch (e) {
+      console.error(e)
+    }
+  }
+}))
+
+export { useEntityStore }
