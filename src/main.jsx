da96eb4732c7082bed244170e452fe36975bdf12
István Ábrahám <istvan.abraham@vicampo.de>
2024-08-27 13:58:42 +0200

    Remove loading test code

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-console.log('Loading')
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
