cedd35a305c0b60ac51d87ae78a3f9ca779b763a
Istvan Abraham <mancsego@gmail.com>
2025-02-19 10:23:02 +0530

    Upgrade dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
-import path from 'path'
-
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: '/randomizer',
   resolve: {
     alias: {
