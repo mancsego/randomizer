81d3cdc377d66aa3a1f9186de6fea73e98037ff8
Istvan Abraham <mancsego@gmail.com>
2025-02-19 10:23:02 +0530

    Upgrade dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 
-import prose from '@tailwindcss/typography'
-
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {}
-  },
-  plugins: [prose]
+  }
 }
